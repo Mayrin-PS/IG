9e383a436d75676347966ee6bf35e34d6bf4820e
Mayrin-PS <mayrinpsunga@gmail.com>
2022-09-30 15:34:54 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        fb: "#365899",
+      },
+    },
+  },
+  plugins: [],
+}
